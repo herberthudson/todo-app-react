e72dbd666ccf2f39985c570dcd47157733f694d1
Herbert <herbert.trindade@businessintegrator.com.br>
2019-10-29 17:46:12 -0200

    adicionando remover pesquisar e marcado como feito

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -16,8 +16,26 @@ export default class Todo extends Component{
             list: []
         }
 
-        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({...this.state, description, list: resp.data}))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
     }
 
     handleChange(e) {
@@ -27,19 +45,46 @@ export default class Todo extends Component{
     handleAdd() {
         const description = this.state.description
         axios.post(URL, {description})
-            .then(resp => console.log('Funcionou'))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsDone(todo){
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPending(todo){
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleClear() {
+        this.refresh()
     }
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm description={this.state.description}
-                    handleChange={this.handleChange}
+                
+                <TodoForm 
+                    description={this.state.description}
                     handleAdd={this.handleAdd}
-
-                />
-                <TodoList />
+                    handleChange={this.handleChange} 
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
+                    />
+                
+                <TodoList 
+                    list={this.state.list} 
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                    handleRemove={this.handleRemove} />
             </div>
         )
     }
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -16,7 +16,11 @@ export default props => (
 
         <Grid cols='12 3 2'>
             <IconButton style="primary" icon='plus' 
-                onClick={props.handleAdd}    ></IconButton>
+                onClick={props.handleAdd}></IconButton>
+            <IconButton style='info' icon='search'
+                onClick={props.handleSearch} />
+            <IconButton style='default' icon='close'
+                onClick={props.handleChange} />
         </Grid>
 
     </div>
